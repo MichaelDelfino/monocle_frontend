8062ad8134d597436b69b184af43da5f2cabcc4b
Michael Delfino <m_delfino@outlook.com>
2022-05-30 10:24:00 -0400

    and another one

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './app.js';
+import App from './App';
 import bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
 const root = createRoot(document.getElementById('root'));
