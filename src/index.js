a5bb520f04c3e96ffedb99daa3e39b039499b56b
Michael Delfino <m_delfino@outlook.com>
2022-05-30 10:21:40 -0400

    fixed app not found

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './app';
+import App from './app.js';
 import bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
 const root = createRoot(document.getElementById('root'));
