bc60cfeb9abbbd162a54470a3aa1f5501b62bbed
mdelfino <mdelfino@winbrogroup.com>
2022-06-01 11:19:10 -0400

    tol lines on mach display

--- src/components/MachineDisplay.js
+++ src/components/MachineDisplay.js
@@ -1,171 +1,171 @@
-import React, { useEffect, useState } from 'react';
-import BoxPlots from './BoxPlots';
+import React, { useEffect, useState } from "react";
+import BoxPlots from "./BoxPlots";
 
 export default function MachineDisplay({ searchHandler, machine }) {
   const [partData, setPartData] = useState({
     parts: [],
     machine: machine,
+    partType: "369P-01",
     numOfParts: 5,
-    metric: 'Diameter',
-    side: 'c-side',
+    metric: "Diameter",
+    side: "c-side",
     startDate: Date.now(),
-    tols: {}
+    tols: {},
   });
 
   useEffect(() => {
-    fetch(
-      `https://salty-inlet-93542.herokuapp.com/parts/?machine=${partData.machine}&timestamp=${partData.startDate}`
-    )
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        const tolPromise = getTextFileSpecs(partData.parts[0])
-        tolPromise.then((specs, tols) => {
+    const getPartTols = async (currentType) => {
+      console.log(currentType);
+      const defFile = "./config/partDefinitions.json";
+      let tolerances = {};
+
+      const response = await fetch(defFile);
+      const partDef = await response.json();
+
+      for (const part of partDef) {
+        if (String(part.partType).trim() === String(currentType).trim()) {
+          tolerances = part.tolerances;
+        }
+      }
+      fetch(
+        `https://salty-inlet-93542.herokuapp.com/parts/?machine=${partData.machine}&parttype=${partData.partType}&timestamp=${partData.startDate}&flag=mach-page`
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
           setPartData({
             parts: data,
             machine: partData.machine,
+            partType: partData.partType,
             numOfParts: partData.numOfParts,
             metric: partData.metric,
             side: partData.side,
             startDate: partData.startDate,
-            tols: tols
+            tols: tolerances,
           });
         })
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, [partData.machine, partData.startDate]);
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    // ****Rename this function
+    getPartTols(partData.partType);
+  }, [partData.machine, partData.startDate, partData.partType]);
 
-  const setMachine = e => {
-    setPartData(prevState => {
+  const setMachine = (e) => {
+    setPartData((prevState) => {
       return { ...prevState, machine: e.target.value };
     });
   };
 
-  const setNumOfParts = e => {
+  const setNumOfParts = (e) => {
     let value = e.target.value;
     if (value > 9) {
-      console.log('bigboi');
+      console.log("bigboi");
       value = 9;
     } else if (value < 1) {
       value = 1;
     }
 
-    setPartData(prevState => {
+    setPartData((prevState) => {
       return { ...prevState, numOfParts: value };
     });
   };
 
-  const setSide = e => {
+  const setSide = (e) => {
     const side = e.target.value;
-    setPartData(prevState => {
+    setPartData((prevState) => {
       return { ...prevState, side: side };
     });
   };
 
-  const setMetric = e => {
+  const setMetric = (e) => {
     const metric = e.target.value;
-    setPartData(prevState => {
+    setPartData((prevState) => {
       return { ...prevState, metric: metric };
     });
   };
 
-  const setStartDate = e => {
-    setPartData(prevState => {
+  const setStartDate = (e) => {
+    setPartData((prevState) => {
       return { ...prevState, startDate: Date.parse(e.target.value) };
     });
   };
 
   // alter time with slider for currently selected day
-  const setStartTime = e => {
+  const setStartTime = (e) => {
     const currentDate = Date.parse(partData.startDate);
     console.log(currentDate);
-    setPartData(prevState => {
+    setPartData((prevState) => {
       return { ...prevState, startDate: Date.parse(e.target.value) };
     });
   };
 
-  const getPartColor = partType => {
-    let borderColor = '';
-    let backgroundColor = '';
+  const getPartColor = (partType) => {
+    let borderColor = "";
+    let backgroundColor = "";
 
-    if (String(partType).trim() === '369P-01') {
-      borderColor = 'rgb(252, 186, 3, 1)';
-      backgroundColor = 'rgb(252, 186, 3, .2)';
-    } else if (String(partType).trim() === '1789P-01') {
-      borderColor = 'rgb(2, 117, 216, 1)';
-      backgroundColor = 'rgb(2, 117, 216, .2)';
-    } else if (String(partType).trim() === '2078P-01') {
-      borderColor = 'rgb(92, 184, 92, 1)';
-      backgroundColor = 'rgb(92, 184, 92, .2)';
-    } else if (String(partType).trim() === '1534P-01') {
-      borderColor = 'rgb(219, 112, 4, 1)';
-      backgroundColor = 'rgb(219, 112, 4, .2)';
-    } else if (String(partType).trim() === '1557P-01') {
-      borderColor = 'rgb(68, 242, 207, 1)';
-      backgroundColor = 'rgb(68, 242, 207, .2)';
-    } else if (String(partType).trim() === '2129P-01') {
-      borderColor = 'rgb(252, 3, 102, 1)';
-      backgroundColor = 'rgb(252, 3, 102, .2)';
-    } else if (String(partType).trim() === '2129P-02') {
-      borderColor = 'rgb(175, 104, 252, 1)';
-      backgroundColor = 'rgb(175, 104, 252, .2)';
-    } else if (String(partType).trim() === '2129P-03') {
-      borderColor = 'rgb(1, 0, 3, 1)';
-      backgroundColor = 'rgb(1, 0, 3, .2)';
-    } else if (String(partType).trim() === '1565P-01') {
-      borderColor = 'rgb(171, 194, 21, 1)';
-      backgroundColor = 'rgb(171, 194, 21, .2)';
+    if (String(partType).trim() === "369P-01") {
+      borderColor = "rgb(252, 186, 3, 1)";
+      backgroundColor = "rgb(252, 186, 3, .2)";
+    } else if (String(partType).trim() === "1789P-01") {
+      borderColor = "rgb(2, 117, 216, 1)";
+      backgroundColor = "rgb(2, 117, 216, .2)";
+    } else if (String(partType).trim() === "2078P-01") {
+      borderColor = "rgb(92, 184, 92, 1)";
+      backgroundColor = "rgb(92, 184, 92, .2)";
+    } else if (String(partType).trim() === "1534P-01") {
+      borderColor = "rgb(219, 112, 4, 1)";
+      backgroundColor = "rgb(219, 112, 4, .2)";
+    } else if (String(partType).trim() === "1557P-01") {
+      borderColor = "rgb(68, 242, 207, 1)";
+      backgroundColor = "rgb(68, 242, 207, .2)";
+    } else if (String(partType).trim() === "2129P-01") {
+      borderColor = "rgb(252, 3, 102, 1)";
+      backgroundColor = "rgb(252, 3, 102, .2)";
+    } else if (String(partType).trim() === "2129P-02") {
+      borderColor = "rgb(175, 104, 252, 1)";
+      backgroundColor = "rgb(175, 104, 252, .2)";
+    } else if (String(partType).trim() === "2129P-03") {
+      borderColor = "rgb(1, 0, 3, 1)";
+      backgroundColor = "rgb(1, 0, 3, .2)";
+    } else if (String(partType).trim() === "1565P-01") {
+      borderColor = "rgb(171, 194, 21, 1)";
+      backgroundColor = "rgb(171, 194, 21, .2)";
     }
 
     return [borderColor, backgroundColor];
   };
 
-  const getTodayFormattedString = date => {
+  const getTodayFormattedString = (date) => {
     // req format: 2022-05-23T16:46
     const origDate = new Date(date);
-    const stringDate = origDate.toLocaleDateString('en-US', {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
+    const stringDate = origDate.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
     });
     const stringTime = origDate
-      .toLocaleTimeString('en-US', {
+      .toLocaleTimeString("en-US", {
         hour12: true,
-        hour: '2-digit',
-        minute: '2-digit',
+        hour: "2-digit",
+        minute: "2-digit",
       })
-      .replace('AM', '')
-      .replace('PM', '')
+      .replace("AM", "")
+      .replace("PM", "")
       .trim();
-    const splitDate = stringDate.split('/');
-    const splitTime = stringTime.split(':');
+    const splitDate = stringDate.split("/");
+    const splitTime = stringTime.split(":");
     const todayDefault = `${splitDate[2]}-${splitDate[0]}-${splitDate[1]}T${splitTime[0]}:${splitTime[1]}`;
     return todayDefault;
   };
 
-
-  const getTextFileSpecs = async (currentType) => {
-    console.log(currentType)
-    const defFile = "./config/partDefinitions.json";
-
-    const response = await fetch(defFile);
-    const partDef = await response.json();
-
-    for (const part of partDef) {
-      if (
-        String(part.partType).trim() === String(currentType).trim()
-      ) {
-        let textFileSpecs,
-          tolerances = {};
-        textFileSpecs = part.textFileSpecs;
-        tolerances = part.tolerances;
-        console.log(textFileSpecs, tolerances)
-        return [textFileSpecs, tolerances];
-      }
-    }
+  const setPartType = (e) => {
+    const partType = e.target.value;
+    setPartData((prevState) => {
+      return { ...prevState, partType: partType };
+    });
   };
 
   return (
@@ -174,11 +174,14 @@ export default function MachineDisplay({ searchHandler, machine }) {
         <div className="machine-info">
           <p className="display-4 lead">
             {partData.machine}
-            <span style={{ color: 'rgb(39, 97, 204)' }}> &nbsp;| &nbsp;</span>
+            <span style={{ color: "rgb(39, 97, 204)" }}> &nbsp;| &nbsp;</span>
           </p>
           <p className="display-4 lead">
-            {partData.side[0].toUpperCase() + partData.side.substring(1,2) + partData.side[2].toUpperCase() + partData.side.substring(3)}
-            <span style={{ color: 'rgb(39, 97, 204)' }}> &nbsp;| &nbsp;</span>
+            {partData.side[0].toUpperCase() +
+              partData.side.substring(1, 2) +
+              partData.side[2].toUpperCase() +
+              partData.side.substring(3)}
+            <span style={{ color: "rgb(39, 97, 204)" }}> &nbsp;| &nbsp;</span>
           </p>
           <p className="display-4 lead">{partData.metric} </p>
         </div>
@@ -217,6 +220,17 @@ export default function MachineDisplay({ searchHandler, machine }) {
                   <option value="WAM 902">WAM 902</option>
                   <option value="WAM 903">WAM 903</option>
                   <option value="WAM 951">WAM 951</option>
+                </select>
+                <select
+                  name="parttype-select"
+                  className="form-select form-select-lg mb-3"
+                  aria-label=".form-select-lg example"
+                  onChange={setPartType}
+                >
+                  <option value="369P-01">369P-01</option>
+                  <option value="1789P-01">1789P-01</option>
+                  <option value="2078P-01">2078P-01</option>
+                  <option value="1565P-01">1565P-01</option>
                 </select>
                 <select
                   className="form-select form-select-lg mb-3"
@@ -261,6 +275,7 @@ export default function MachineDisplay({ searchHandler, machine }) {
                 metric={partData.metric}
                 searchHandler={searchHandler}
                 tols={partData.tols}
+                parttype={partData.partType}
               />
             </div>
           </div>
