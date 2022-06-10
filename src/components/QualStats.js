3d2b843191db4a17c2d1425fad448c808572d6ae
mdelfino <mdelfino@winbrogroup.com>
2022-06-10 15:17:07 -0400

    started yield page

---
+++ src/components/QualStats.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function QualStats() {
+  return (
+    <div className="Stats-Display">
+      <div id="stats-title" className="jumbotron stats-jumbotron">
+        <div className="stats-info">
+          <p className="display-4 lead">
+            Quality Statistics
+            <span style={{ color: "rgb(39, 97, 204)" }}> &nbsp;| &nbsp;</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
