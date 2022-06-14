66887cc3074993c8f8f729703adc3bcf8bf5d969
mdelfino <mdelfino@winbrogroup.com>
2022-06-14 13:49:34 -0400

    feat: partial pass/fail display

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,74 +1,52 @@
-import React, { useEffect, useState } from 'react';
-import { Doughnut } from 'react-chartjs-2';
-import { Chart, registerables } from 'chart.js';
-import zoomPlugin from 'chartjs-plugin-zoom';
+import React, { useEffect, useState } from "react";
+import { Doughnut } from "react-chartjs-2";
+import { Chart, registerables } from "chart.js";
+import zoomPlugin from "chartjs-plugin-zoom";
 Chart.register(...registerables, zoomPlugin);
 
-export const PieChart = props => {
-  const [partData, setPartData] = useState();
+export default function PieChart({ passedParts, failedParts }) {
+  const [partData, setPartData] = useState(null);
 
   useEffect(() => {
-    fetchParts();
-  }, []);
+    setDatasets(passedParts, failedParts);
+  }, [passedParts, failedParts]);
 
-  // Todo - Make this part fetch modular,
-  //        each component uses the same code
-  const fetchParts = async (machine = 0) => {
-    const res = await fetch('./part.json');
-    const data = await res.json();
-    console.log(data);
-
-    const [machineNums, partTypes] = getPartTypesAndMachines(data);
-    // const [borderColor, backgroundColor] = getPartColor(data);
-    console.log(partTypes);
-    console.log(machineNums);
-
+  const setDatasets = (passedParts, failedParts) => {
+    console.log(passedParts, failedParts);
     setPartData({
-      labels: partTypes,
-      machine: machineNums[machine],
+      labels: ["Pass", "Fail"],
       datasets: [
         {
-          label: 'Type Distribution',
-          data: [300, 50, 100],
-          backgroundColor: [
-            'rgb(240, 173, 78, .2)',
-            'rgb(2, 117, 216, .2)',
-            'rgb(92, 184, 92, .2)',
-          ],
-          borderColor: [
-            'rgb(240, 173, 78, 1)',
-            'rgb(2, 117, 216, 1)',
-            'rgb(92, 184, 92, 1)',
-          ],
-          hoverBackgroundColor: [
-            'rgb(240, 173, 78, 1)',
-            'rgb(2, 117, 216, 1)',
-            'rgb(92, 184, 92, 1)',
-          ],
-          hoverBorderColor: [
-            'rgb(240, 173, 78, 1)',
-            'rgb(2, 117, 216, 1)',
-            'rgb(92, 184, 92, 1)',
-          ],
+          label: "Pass Fail Distribution",
+          data: [passedParts.length, failedParts.length],
+          backgroundColor: ["rgb(38, 240, 72, .2)", "rgb(235, 14, 14, .2)"],
+          borderColor: ["rgb(38, 240, 72, 1)", "rgb(235, 14, 14, 1)"],
+          hoverBackgroundColor: ["rgb(38, 240, 72, 1)", "rgb(235, 14, 14, 1)"],
+          hoverBorderColor: ["rgb(38, 240, 72, 1)", "rgb(235, 14, 14, 1)"],
           hoverOffset: 4,
         },
       ],
     });
   };
 
-  const getPartTypesAndMachines = data => {
-    const machines = data.map(data => data.machine);
-    const partTypes = data.map(data => data.partType);
-    return [machines, partTypes];
-  };
-
   return (
-    <div>
-      {props ? <p>WAM {props.machine}</p> : 'no data'}
-      <p></p>
+    <div className="pie-chart">
       <div className="doughnut">
-        {partData ? <Doughnut data={partData} /> : 'no data available'}
+        {partData ? (
+          <Doughnut
+            data={partData}
+            options={{
+              plugins: {
+                legend: {
+                  display: false,
+                },
+              },
+            }}
+          />
+        ) : (
+          "no data available"
+        )}
       </div>
     </div>
   );
-};
+}
--- src/components/QualStats.js
+++ src/components/QualStats.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
+import PieChart from "./PieChart";
 
 // Use this component as base for modularization of "get" functions
 
 export default function QualStats() {
   const [partData, setPartData] = useState({
     startDate: Date.now(),
-    endDate: Date.now() - 604800000,
+    endDate: Date.now() - 86400000,
+    totalParts: [],
+    passedParts: [],
+    failedParts: [],
+    period: "Today",
   });
 
   useEffect(() => {
@@ -23,37 +28,36 @@ export default function QualStats() {
         })
         .then(data => {
           console.log(data);
-          calcPassFail(data).then(response => {
-            console.log(response);
-          });
+          calcPassFail(data);
         })
         .catch(error => {
-          if (error.name === 'AbortError') {
+          if (error.name === "AbortError") {
             console.log(error);
           }
         });
     };
 
     const calcPassFail = async data => {
-      const totalParts = data.length;
+      const totalParts = data;
 
       let allCDia,
         allADia,
         allCPos,
-        allAPos,
-        passedParts,
-        failedParts = [];
+        allAPos = [];
+
+      let passedParts = [];
+      let failedParts = [];
 
       let tolerances = {};
 
       for (const part of data) {
-        const defFile = './config/partDefinitions.json';
+        const defFile = "./config/partDefinitions.json";
         const response = await fetch(defFile);
         const partDef = await response.json();
 
         for (const def of partDef) {
           if (String(def.partType).trim() === String(part.parttype).trim()) {
-            tolerances = part.tolerances;
+            tolerances = def.tolerances;
           }
         }
         allCDia = getCDiameters(part);
@@ -63,16 +67,36 @@ export default function QualStats() {
 
         if (
           Math.max(...allCDia) >
-            tolerances['c-side']['diaNom'] + tolerances['c-side']['diaPlus'] ||
-          Math.max(...allCDia) <
-            tolerances['c-side']['diaNom'] - tolerances['c-side']['diaMin']
+            tolerances["c-side"]["diaNom"] + tolerances["c-side"]["diaPlus"] ||
+          Math.min(...allCDia) <
+            tolerances["c-side"]["diaNom"] - tolerances["c-side"]["diaMin"] ||
+          Math.max(...allADia) >
+            tolerances["a-side"]["diaNom"] + tolerances["a-side"]["diaPlus"] ||
+          Math.min(...allADia) <
+            tolerances["a-side"]["diaNom"] - tolerances["a-side"]["diaMin"] ||
+          Math.max(...allCPos) >
+            tolerances["c-side"]["posNom"] + tolerances["c-side"]["posPlus"] ||
+          Math.min(...allCPos) <
+            tolerances["c-side"]["posNom"] - tolerances["c-side"]["posMin"] ||
+          Math.max(...allAPos) >
+            tolerances["a-side"]["posNom"] + tolerances["a-side"]["posPlus"] ||
+          Math.min(...allAPos) <
+            tolerances["a-side"]["posNom"] - tolerances["a-side"]["posMin"]
         ) {
           failedParts.push(part);
         } else {
           passedParts.push(part);
         }
       }
-      console.log(totalParts, passedParts.length, failedParts.length);
+      console.log(totalParts, passedParts, failedParts);
+      setPartData(prevState => {
+        return {
+          ...prevState,
+          totalParts: totalParts,
+          passedParts: passedParts,
+          failedParts: failedParts,
+        };
+      });
     };
 
     getWeekData(partData.startDate, partData.endDate);
@@ -116,16 +140,267 @@ export default function QualStats() {
     return positionArray;
   };
 
+  const setPeriod = e => {
+    const period = e.target.value;
+    let timeOffset = 0;
+    if (partData.period === "Today") {
+      timeOffset = 604800000;
+    } else {
+      timeOffset = 86400000;
+    }
+    setPartData(prevState => {
+      return {
+        ...prevState,
+        endDate: Date.now() - timeOffset,
+        period: period,
+      };
+    });
+  };
+
   return (
     <div className="Stats-Display">
       <div id="stats-title" className="jumbotron stats-jumbotron">
         <div className="stats-info">
           <p className="display-4 lead">
             Quality Statistics
-            <span style={{ color: 'rgb(39, 97, 204)' }}></span>
+            <span style={{ color: "rgb(39, 97, 204)" }}></span>
           </p>
         </div>
       </div>
+      <select onChange={setPeriod} defaultValue={partData.period}>
+        <option value="This Week">this week</option>
+        <option value="Today">today</option>
+      </select>
+      {/* Conditionally render each part type's stats based on data within time period */}
+      {/* Need to try to make the part types dynamic, adding a new part type will require hard coding a new section */}
+      <div className="stats-time-period display-5 lead">{partData.period}</div>
+      {partData.totalParts.length ? (
+        <div className="stats-totals">
+          <div className="totals-column">
+            <p className="display-6 lead">Totals</p>
+            <div>Total: {partData.totalParts.length}</div>
+            <div>Passed: {partData.passedParts.length}</div>
+            <div>Failed: {partData.failedParts.length}</div>
+            <div>
+              <PieChart
+                passedParts={partData.passedParts}
+                failedParts={partData.failedParts}
+              />
+            </div>
+          </div>
+          {partData.totalParts.filter(part => part.parttype === "369P-01")
+            .length ? (
+            <div className="parttype-column">
+              <p className="display-6 lead">369's</p>
+              <div>
+                Total:{" "}
+                {
+                  partData.totalParts.filter(part => part.parttype == "369P-01")
+                    .length
+                }
+              </div>
+              <div>
+                Passed:{" "}
+                {
+                  partData.passedParts.filter(
+                    part => part.parttype == "369P-01"
+                  ).length
+                }
+              </div>
+              <div>
+                Failed:{" "}
+                {
+                  partData.failedParts.filter(
+                    part => part.parttype == "369P-01"
+                  ).length
+                }
+              </div>
+              <div>
+                <PieChart
+                  passedParts={partData.passedParts.filter(
+                    part => part.parttype === "369P-01"
+                  )}
+                  failedParts={partData.failedParts.filter(
+                    part => part.parttype === "369P-01"
+                  )}
+                />
+              </div>
+            </div>
+          ) : (
+            <div></div>
+          )}
+          {partData.totalParts.filter(part => part.parttype === "1789P-01")
+            .length ? (
+            <div className="parttype-column">
+              <p className="display-6 lead">1789's</p>
+              <div>
+                Total:{" "}
+                {
+                  partData.totalParts.filter(
+                    part => part.parttype == "1789P-01"
+                  ).length
+                }
+              </div>
+              <div>
+                Passed:{" "}
+                {
+                  partData.passedParts.filter(
+                    part => part.parttype == "1789P-01"
+                  ).length
+                }
+              </div>
+              <div>
+                Failed:{" "}
+                {
+                  partData.failedParts.filter(
+                    part => part.parttype == "1789P-01"
+                  ).length
+                }
+              </div>
+              <div>
+                <PieChart
+                  passedParts={partData.passedParts.filter(
+                    part => part.parttype === "1789P-01"
+                  )}
+                  failedParts={partData.failedParts.filter(
+                    part => part.parttype === "1789P-01"
+                  )}
+                />
+              </div>
+            </div>
+          ) : (
+            <div></div>
+          )}
+          {partData.totalParts.filter(part => part.parttype === "2078P-01")
+            .length ? (
+            <div className="parttype-column">
+              <p className="display-6 lead">2078's</p>
+              <div>
+                Total:{" "}
+                {
+                  partData.totalParts.filter(
+                    part => part.parttype == "2078P-01"
+                  ).length
+                }
+              </div>
+              <div>
+                Passed:{" "}
+                {
+                  partData.passedParts.filter(
+                    part => part.parttype == "2078P-01"
+                  ).length
+                }
+              </div>
+              <div>
+                Failed:{" "}
+                {
+                  partData.failedParts.filter(
+                    part => part.parttype == "2078P-01"
+                  ).length
+                }
+              </div>
+              <div>
+                <PieChart
+                  passedParts={partData.passedParts.filter(
+                    part => part.parttype === "2078P-01"
+                  )}
+                  failedParts={partData.failedParts.filter(
+                    part => part.parttype === "2078P-01"
+                  )}
+                />
+              </div>
+            </div>
+          ) : (
+            <div></div>
+          )}
+          {partData.totalParts.filter(part => part.parttype === "1565P-01")
+            .length ? (
+            <div className="parttype-column">
+              <p className="display-6 lead">1565's</p>
+              <div>
+                Total:{" "}
+                {
+                  partData.totalParts.filter(
+                    part => part.parttype == "1565P-01"
+                  ).length
+                }
+              </div>
+              <div>
+                Passed:{" "}
+                {
+                  partData.passedParts.filter(
+                    part => part.parttype == "1565P-01"
+                  ).length
+                }
+              </div>
+              <div>
+                Failed:{" "}
+                {
+                  partData.failedParts.filter(
+                    part => part.parttype == "1565P-01"
+                  ).length
+                }
+              </div>
+              <div>
+                <PieChart
+                  passedParts={partData.passedParts.filter(
+                    part => part.parttype === "1565P-01"
+                  )}
+                  failedParts={partData.failedParts.filter(
+                    part => part.parttype === "1565P-01"
+                  )}
+                />
+              </div>
+            </div>
+          ) : (
+            <div></div>
+          )}
+          {partData.totalParts.filter(part => part.parttype === "1787P-01")
+            .length ? (
+            <div className="parttype-column">
+              <p className="display-6 lead">1787's</p>
+              <div>
+                Total:{" "}
+                {
+                  partData.totalParts.filter(
+                    part => part.parttype == "1787P-01"
+                  ).length
+                }
+              </div>
+              <div>
+                Passed:{" "}
+                {
+                  partData.passedParts.filter(
+                    part => part.parttype == "1787P-01"
+                  ).length
+                }
+              </div>
+              <div>
+                Failed:{" "}
+                {
+                  partData.failedParts.filter(
+                    part => part.parttype == "1787P-01"
+                  ).length
+                }
+              </div>
+              <div>
+                <PieChart
+                  passedParts={partData.passedParts.filter(
+                    part => part.parttype === "1787P-01"
+                  )}
+                  failedParts={partData.failedParts.filter(
+                    part => part.parttype === "1787P-01"
+                  )}
+                />
+              </div>
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 }
