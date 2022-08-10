e65f695acacc6bb9a4bb092dbe0dfef9a4642386
mdelfino <mdelfino@winbrogroup.com>
2022-08-10 13:20:45 -0400

    temp

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -24,12 +24,28 @@ export const SearchBar = ({ searchHandler }) => {
     }
     e.target.classList.toggle("link-dark");
   };
+
   return (
     <nav className="navbar navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          Winbro - Monocle
-        </a>
+        <div className="menu-items">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="25"
+            height="25"
+            fill="currentColor"
+            className="menu-button bi bi-list"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fillRule="evenodd"
+              d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+            />
+          </svg>
+          <a className="navbar-brand" href="/">
+            Winbro - Monocle
+          </a>
+        </div>
 
         <form className="d-flex" onSubmit={setSearchData}>
           <input
@@ -46,85 +62,5 @@ export const SearchBar = ({ searchHandler }) => {
         </form>
       </div>
     </nav>
-    // <nav className="navbar navbar-light bg-light">
-    //   <div className="container-fluid">
-    //     <a className="navbar-brand" href="#">
-    //       Winbro - Monocle
-    //     </a>
-    //     <button
-    //       className="navbar-toggler"
-    //       type="button"
-    //       data-bs-toggle="offcanvas"
-    //       data-bs-target="#offcanvasNavbar"
-    //       aria-controls="offcanvasNavbar"
-    //     >
-    //       <span className="navbar-toggler-icon"></span>
-    //     </button>
-    //     <div
-    //       className="offcanvas offcanvas-end"
-    //       tabindex="-1"
-    //       id="offcanvasNavbar"
-    //       aria-labelledby="offcanvasNavbarLabel"
-    //     >
-    //       <div className="offcanvas-header">
-    //         <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
-    //           Main Menu
-    //         </h5>
-    //         <button
-    //           type="button"
-    //           className="btn-close text-reset"
-    //           data-bs-dismiss="offcanvas"
-    //           aria-label="Close"
-    //         ></button>
-    //       </div>
-    //       <div className="offcanvas-body">
-    //         <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-    //           <li className="nav-item">
-    //             <a
-    //               className="nav-link active"
-    //               aria-current="page"
-    //               onMouseEnter={toggleHighlight}
-    //               onMouseLeave={toggleHighlight}
-    //               onClick={props.sectionHandler.bind(null, 'home')}
-    //             >
-    //               Home
-    //             </a>
-    //           </li>
-    //           <li className="nav-item">
-    //             <a
-    //               className="nav-link"
-    //               onMouseEnter={toggleHighlight}
-    //               onMouseLeave={toggleHighlight}
-    //               onClick={props.sectionHandler.bind(null, 'part')}
-    //             >
-    //               Part Display
-    //             </a>
-    //           </li>
-    //           <li className="nav-item dropdown">
-    //             <a
-    //               className="nav-link"
-    //               onMouseEnter={toggleHighlight}
-    //               onMouseLeave={toggleHighlight}
-    //               onClick={props.sectionHandler.bind(null, 'mach')}
-    //             >
-    //               Machine Display
-    //             </a>
-    //           </li>
-    //         </ul>
-    //         <form className="d-flex" onSubmit={setSearchData}>
-    //           <input
-    //             className="form-control me-2"
-    //             type="search"
-    //             placeholder="Search"
-    //             aria-label="Search"
-    //           />
-    //           <button className="btn btn-outline-primary" type="submit">
-    //             Search
-    //           </button>
-    //         </form>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </nav>
   );
 };
